3e45ebd40294f92e5b00a28fd105370c71846230
devops793792 <122431959+devops793792@users.noreply.github.com>
2023-09-11 20:23:22 +0530

    Update __init__.py

--- hello_app/__init__.py
+++ hello_app/__init__.py
@@ -4,5 +4,4 @@ from flask import Flask  # Import the Flask class
 app = Flask(__name__)    # Create an instance of the class for our use
 from flask import Flask  # Import the Flask class
 app = Flask(__name__)    # Create an instance of the class for our use
-from flask import Flask  # Import the Flask class
-app = Flask(__name__)    # Create an instance of the class for our use
+
